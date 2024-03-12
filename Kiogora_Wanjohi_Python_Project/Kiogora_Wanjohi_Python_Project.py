08d8de91d8f2f8a26434c91c7cc3c5cfd90a1594
kibet-wilfred <wikiearapkoech@gmail.com>
2024-03-12 14:14:30 +0300

    first commit

---
+++ Kiogora_Wanjohi_Python_Project/Kiogora_Wanjohi_Python_Project.py
@@ -0,0 +1,12 @@
+#single line comment
+"""
+#multiline comment
+#multiline comment
+#multiline comment
+#multiline commgient
+#multiline comment
+#multiline comment
+#multiline comment
+#multiline comment
+"""
+x
