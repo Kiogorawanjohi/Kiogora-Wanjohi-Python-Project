1247c2306778f55a769ea02558607ddda6757162
kibet-wilfred <wikiearapkoech@gmail.com>
2024-03-15 16:31:41 +0300

    unsolved problem concerning entering names task

--- Kiogora_Wanjohi_Python_Project/Kiogora_Wanjohi_Python_Project.py
+++ Kiogora_Wanjohi_Python_Project/Kiogora_Wanjohi_Python_Project.py
@@ -97,23 +97,74 @@ while x<=10:
     else:sum=sum+x
     x=x+1
 else:
-    print(sum)"""
+    print(sum)
 
 #program regarding visitors
 visitors_number=0
-ugandan=0
-kenyan=0
+ugandans=0
+kenyans=0
 count=0
 print("hello i am Kiogora Wanjohi thankyou for choosing my visitors platform")
 visitors_number=int(input("enter the total number of visitor"))
 while visitors_number>count:
     country=input("which country are you from?")
-    if country=="uganda":
+    if country.lower()=="uganda":
         ugandans=ugandans+1
         count=count+1
-    elif:country=="kenyan":
-         kenyan=kenyan+1
-         count=count+1
-else:print("your country is not eligible for this service")
-     print("the total number of ugandans is:"+ ugandans)
-     print("the total number of kenyans is:"+ kenyans)
+    elif country.lower()=="kenya":
+        kenyans=kenyans+1
+        count=count+1
+print("the total number of ugandans is:"+ str(ugandans))
+print("the total number of kenyans is:"+ str(kenyans))
+
+#defining my function to add odd and even numbers
+def odd_add_check(first,last):
+    sum = 0
+    for number in range(first,last+1):
+        if number%2==0:
+            continue
+        else:sum+=number
+    return sum
+
+result=odd_add_check(2,34)
+print(result)
+
+#sum of all even numbers between 1 and 10
+sum=0
+for number in range(1,11):
+    if number%2==0:
+        sum+=number
+
+print(sum)
+
+#creating and modifying lists
+details=["Jack","Isaac","Kiogora","wanjohi",13790]
+print(details)
+details.append("Nyeri high school")
+print(details)
+details.extend([4,5])
+print(details)
+
+#tasking to enter names of students in a list and print out
+names=[]
+student1=input("student1 kindly enter your name")
+print("you have entered the name",student1)
+names.append(student1)
+student2=input("student2 kindly enter your name")
+print("you have entered the name",student2)
+names.append(student2)
+student3=input("student3 kindly enter your name")
+print("you have entered the name",student3)
+names.append(student3)
+print(names)
+
+names=[]
+totalnumber=int(input("enter the number of names you want to enter"))
+for name in range(totalnumber+1):
+    names.append(input(name))
+    print("you have entered the name",name)
+
+
+    print(name)
+    """
+
